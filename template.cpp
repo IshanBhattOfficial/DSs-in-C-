b8b69e1a6eef97f313590cca008b419f8d7da867
Ishan Bhatt <ishanbhattofficial@gmail.com>
2017-03-14 20:41:22 +0530

    this is a template

---
+++ template.cpp
@@ -0,0 +1,27 @@
+/*	Author information and 
+	date of creation
+	brief description of the code */
+
+
+//libraries or dependencies
+//compile time instructions
+#include<iostream>
+using namespace std;
+
+class sample{
+	public:
+		sample(){
+			cout<<"\nThis is a sample.\n";
+		}
+//try not to use inline functions
+};
+//function definitions
+
+int main(int argc, char* argv[]){
+	sample s;
+//try to use all functions
+//try to deal with all the cases
+	return 0;
+}
+
+
